935c1affb7e02aadbeb0b697ae277ca856af8ce9
dhanushdckap <dhanushgdckap@gmail.com>
2023-10-10 16:33:49 +0530

    finished

--- controller/votes.py
+++ controller/votes.py
@@ -1,20 +1,17 @@
 from main import *
 
-class Vote(DB_Connection.DB_Configuration):
-    def init(self):
-        super().__init__("Movie_management_system")
-
+class Vote():
     def vote_the_movie(self,movie_name,vote,user_id):
         if movie_name == '': # checking the movie is not empty
             return "Movie name doesn't empty"
-        movie = self.vote.find_one({"movie_name": movie_name}) #checking if the movie is already exists.
-        movie_collection = self.movies.find_one({"movie_name": movie_name}) # checking if the given movie is alreafy exists in movies collection.
+        movie = votes.find_one({"movie_name": movie_name}) #checking if the movie is already exists.
+        movie_collection = movies.find_one({"movie_name": movie_name}) # checking if the given movie is alreafy exists in movies collection.
         if movie:
             return "Movie already Exists"
         else:
             if movie_collection: # if given movie is in the movies collection then only we can vote or else error will occurs.
                 vote_data = {"movie_name":movie_name,"vote":vote,"user_id":user_id}
-                self.vote.insert_one(vote_data)
+                votes.insert_one(vote_data)
                 return "Vote inserted Succssfully"
             else:
                 return "Movie doesn't found"
@@ -25,4 +22,4 @@ class Vote(DB_Connection.DB_Configuration):
 
 
 
-vote_object = Vote("Movie_management_system")
+vote_object = Vote()
