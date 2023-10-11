260deb14d873e2b7c45f48bc0f788d66f6377bab
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-10-11 10:27:14 +0530

    Changed the user_id and Movie_id into objectid

--- controller/votes.py
+++ controller/votes.py
@@ -6,10 +6,10 @@ class Vote():
         movie = movies.find_one({"_id": ObjectId(movie_id)}) #checking if the movie is already exists.
 
         if movie:
-            unique_movie = votes.find_one({"movie_id": movie_id,"user_id":user_id}) # checking if the movie is already exists in likes collection.
+            unique_movie = votes.find_one({"movie_id": ObjectId(movie_id),"user_id":ObjectId(user_id)}) # checking if the movie is already exists in likes collection.
 
             if not unique_movie: # if given movie is in the movies collection then only we can vote or else error will occurs.
-                vote_data = {"movie_id":movie_id,"vote":vote,"user_id":user_id}
+                vote_data = {"movie_id":ObjectId(movie_id),"vote":vote,"user_id":ObjectId(user_id)}
                 votes.insert_one(vote_data)
                 return "Vote inserted Succssfully"
             else:
