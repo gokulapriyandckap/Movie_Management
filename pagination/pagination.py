6d2e438c3adf1ba944287a6796ce4bd1995da60d
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-10-16 15:04:22 +0530

    pagination completed need to change the query dynamically

--- pagination/pagination.py
+++ pagination/pagination.py
@@ -1,12 +1,12 @@
 from main import *
 from General_Functions.General_functions import *
-from bson import json_util,ObjectId
+# import requests
 
 class Pagination:
     def __init__(self,limit,page):
         self.limit = int(limit)
         self.page = int(page)
-        self.items_per_page = 4
+        # self.items_per_page = 4
     def data(self): # this function decribes that the feth the data with the criterira pf limit and pages.
 
         skip = (self.page - 1) * self.items_per_page # Calculate the number of items to skip in the pagination based on the current page and items per page.
@@ -25,11 +25,9 @@ class Pagination:
 
 
         items = list(movies.find().skip(skip).limit(self.items_per_page)) # This query fetching the movies with given limit and given page.
-
         for item in items:
             item["_id"] = str(item["_id"])
+            item["user_id"] = str (item["user_id"])
 
         # after getting the all elements calling the pagination_response_data function by respective arguments.
         return  pagination_response_date(data=items,message="Movies Listed successfully", next=self.page + 1,page=self.page, pages=pages, prev=self.page - 1, status=200,success=True, total=total_no_pages(), total_records=total_movies)
-
-
