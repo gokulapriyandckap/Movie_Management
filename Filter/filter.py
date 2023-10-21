90a2f02b7f832ee4759c3e6da80ac538c082f607
Jeeva <jeevasdckap@gmail.com>
2023-10-21 14:36:00 +0530

    show my movie and remove refresh token

--- Filter/filter.py
+++ Filter/filter.py
@@ -4,10 +4,11 @@ from bson import json_util,ObjectId
 
 class filter:
 
-    def __init__(self,args,collection_name):
+    def __init__(self,args,collection_name,get_access):
         self.args = args
         self.query = {}
         self.collection_name = collection_name
+        self.access = get_access
 
     def filter_query_builder(self):
         for key, value in self.args.items():
@@ -15,10 +16,15 @@ class filter:
                 self.query.update({key[7::1]: value})
             elif key == "search":
                 self.search_query_builder()
+
+        if self.access:
+            self.query.update({"user_id":self.access})
+
         criteria = self.collection_name.find(self.query)
         count_documents = self.collection_name.count_documents(self.query)
         data_counts = [criteria, count_documents]
         return data_counts
+        # return self.query
 
     def search_query_builder(self):
         query = {
